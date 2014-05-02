446ad07380e3d0b9f2f5def2a322ee509f5a4e29
Ryan Hitchman <hitchmanr@gmail.com>
2014-05-02 01:49:50 -0700

    sim sketch done. needs test programs

---
+++ sim.py
@@ -0,0 +1,254 @@
+import functools
+
+DIGITS = 'ABCDEFGHIJKLM_NOPQRSTUVWXYZ'
+
+
+def coerce_other(f):
+    def wrapper(self, other, *args):
+        if not isinstance(other, Tryte):
+            other = Tryte(other)
+        return f(self, other, *args)
+    return wrapper
+
+
+@functools.total_ordering
+class Tryte(object):
+
+    def __init__(self, value=0):
+        self.value = (value + 364) % 729 - 364
+
+    @classmethod
+    def from_tribbles(cls, tribbles):
+        assert len(tribbles) == 2
+        mst = DIGITS.index(tribbles[0])
+        lst = DIGITS.index(tribbles[1])
+        return cls((mst * 27 + lst) - 364)
+
+    @classmethod
+    def from_trits(cls, trits):
+        val = 0
+        for trit in trits:
+            val = val * 3 + trit
+        return cls(val - 364)
+
+    @classmethod
+    def from_random(cls):
+        return cls(random.randint(-364, 365))
+
+    def __int__(self):
+        return self.value
+
+    def tribbles(self):
+        mst, lst = divmod(self.value + 364, 27)
+        return mst, lst
+
+    def __str__(self):
+        mst, lst = self.tribbles()
+        return DIGITS[mst] + DIGITS[lst]
+
+    def trits_raw(self):
+        val = self.value + 364
+        ret = []
+        for _ in xrange(6):
+            val, rem = divmod(val, 3)
+            ret.append(rem)
+        return ret[::-1]
+
+    def trits(self):
+        return ''.join('T01'[val] for val in self.trits_raw())
+
+    @coerce_other
+    def __add__(self, other):
+        return Tryte(self.value + other.value)
+
+    @coerce_other
+    def __sub__(self, other):
+        return Tryte(self.value + other.value)
+
+    @coerce_other
+    def logic(self, other, fn):
+        return Tryte.from_trits(fn(a, b) for a, b in
+                                zip(self.trits_raw(), other.trits_raw()))
+
+    def __and__(self, other):
+        return self.logic(other, min)
+
+    def __or__(self, other):
+        return self.logic(other, max)
+
+    @coerce_other
+    def __eq__(self, other):
+        return self.value == other.value
+
+    @coerce_other
+    def __lt__(self, other):
+        return self.value < other.value
+
+
+class Machine(object):
+
+    PC_INDEX = DIGITS.index('P') - 13
+    SP_INDEX = DIGITS.index('S') - 13
+    Z_INDEX = DIGITS.index('Z') - 13
+
+    def __init__(self):
+        self.mem = [Tryte(0) for _ in xrange(729)]
+
+    def load_program(self, prog, offset=-364):
+        prog = ''.join(prog.split()).upper()
+        for x in xrange(0, len(prog), 2):
+            self[offset + x/2] = Tryte.from_tribbles(prog[x:x+2])
+
+    def dump_state(self):
+        print 'Machine state:'
+        print '  ', '  '.join(DIGITS)
+        for row_n in xrange(-13, 14):
+            offset = row_n * 27 - 13
+            row = DIGITS[row_n + 13]
+            for n in xrange(27):
+                row += ' ' + str(self[offset + n])
+            print row
+
+    def __getitem__(self, index):
+        index = int(index)
+        if index == self.Z_INDEX:
+            return Tryte(0)
+        return self.mem[index + 364]
+
+    def __setitem__(self, index, value):
+        assert isinstance(value, Tryte)
+        self.mem[int(index) + 364] = value
+
+    def read_pc(self):
+        ret = self[self.PC_INDEX]
+        self[self.PC_INDEX] += 1
+        return ret
+
+    def step(self):
+        def decode_ref(r):
+            if r == '_':
+                return self.Z_INDEX
+            elif r == 'N':
+                return self.read_pc()
+            return DIGITS.index(r) - 13
+
+        def decode_val(v):
+            if v == '_':
+                return self.read_pc()
+            elif v == 'N':
+                return self[self.read_pc()]
+            return self[DIGITS.index(v) - 13]
+
+        def decode_vals_from_pc():
+            a, b = str(self.read_pc())
+            return decode_val(a), decode_val(b)
+
+        op, low = str(self.read_pc())
+        if op in 'ZOXIDFB':   # 1 operand, write operand
+            ref = decode_ref(low)
+            if op == 'Z':   # Zero
+                self[ref] = Tryte(0)
+            elif op == 'O':  # Pop
+                self[ref] = self[self[self.SP_INDEX]]
+                self[self.SP_INDEX] -= 1
+            elif 'X':  # Swap tribble
+                self[ref] = Tryte.from_tribbles(str(self[ref])[::-1])
+            elif 'I':  # Increment
+                self[ref] += 1
+            elif 'D':  # Decrement
+                self[ref] -= 1
+            elif 'F':  # Rotate right (forward)
+                trits = self[ref].trits_raw()
+                self[ref] = Tryte.from_trits(trits[-1:] + trits[:-1])
+            elif 'B':  # Rotate left (backward)
+                trits = self[ref].trits_raw()
+                self[ref] = Tryte.from_trits(trits[1:] + trits[:1])
+        elif op in 'UC':    # 1 value-only operand
+            val = decode_val(low)
+            if op == 'U':  # Push
+                self[self[self.SP_INDEX]] = val
+                self[self.SP_INDEX] += 1
+            elif 'C':  # Call
+                self[self[self.SP_INDEX]] = self[self.PC_INDEX]
+                self[self.SP_INDEX] += 1
+                self[self.PC_INDEX] = val
+        elif op in 'ASNERT':  # 3 operand
+            dest = decode_ref(low)
+            a, b = decode_vals_from_pc()
+            if op == 'A':    # Add
+                self[dest] = a + b
+            elif op == 'S':  # Sub
+                self[dest] = a - b
+            elif op == 'N':  # And
+                self[dest] = a & b
+            elif op == 'E':  # Or (Either)
+                self[dest] = a | b
+            elif op == 'R':  # Read a+b to dest
+                self[dest] = self[a + b]
+            elif op == 'T':  # Perform tritwise logical operation
+                # actually 4 operand
+                logic_results = self.read_pc().trits_raw()
+                logic_combos = ((0, 0), (0, 1), (0, 2), (1, 1), (1, 2), (2, 2))
+                logic_map = {}
+                for (ta, tb), res in zip(logic_combos, logic_results):
+                    logic_map[ta, tb] = res
+                    logic_map[tb, ta] = res
+                self[dest] = a.logic(b, logic_map.get)
+        elif op == 'W':  # Write value-only to a+b
+            val = decode_val(low)
+            a, b = decode_vals_from_pc()
+            self[a + b] = val
+        elif op in 'JGLQ':  # jumps
+            if low == '_':
+                offset = self.read_pc()
+            else:
+                offset = DIGITS.index(low) - 13
+            if op == 'J':
+                self[self.PC_INDEX] += offset
+            else:
+                a, b = decode_vals_from_pc()
+                if (op == 'G' and a >= b or
+                   op == 'L' and a < b or
+                   op == 'Q' and a == b):
+                        self[self.PC_INDEX] += offset
+        elif op in 'MV':  # special
+            dest = decode_ref(low)
+            val = self.read_pc()
+            if op == 'M':  # move a value in memory to dest
+                self[dest] = self[val]
+            elif op == 'V':  # move a value to dest
+                self[dest] = val
+        else:
+            raise NotImplementedError('OP: %s' % op)
+
+
+for n in xrange(-364, 365):
+    t = Tryte(n)
+    assert n == int(t)
+    assert t == Tryte.from_tribbles(str(t))
+
+import random
+
+
+for _ in xrange(100):
+    a, b = Tryte.from_random(), Tryte.from_random()
+    aandb = a | b
+    print a, b, aandb, a.trits(), b.trits(), aandb.trits()
+
+a = Tryte(0)
+print a, a.value
+
+m = Machine()
+
+random.seed(5)
+for _ in range(1000):
+    prog = ''.join(random.choice(DIGITS) for _ in xrange(729 * 2))
+    m.load_program(prog)
+    m.dump_state()
+    for _ in range(100):
+        try:
+            m.step()
+        except NotImplementedError:
+            print '>>>> NIE'
+            break
+    m.dump_state()
